eb0d7b238be676212099c028a6bf4e44a332bf0c
Bjørn <bvinther@hotmail.com>
2016-11-01 00:15:11 +0100

    added language

---
+++ Assets/Language/Scripts/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SpacedOut.Utils
+{
+    /// <summary>
+    /// Manager scripts should extend this class to implement a singleton pattern
+    /// </summary>
+    /// <typeparam name="T">Type of class</typeparam>
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T instance;
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+            }
+            else
+            {
+                Debug.LogError("Destroying duplicate singeton of type: " + typeof(T));
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using SpacedOut.Utils;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Translator.instance.SetLanguage(Translator.Language.Danish);
+        print(Translator.instance.Get("testing"));
+        print(Translator.instance.Get("title"));
+        print(Translator.instance.Get("text"));
+        print(Translator.instance.Get("play"));
+    }
+}
